a7960d83a2b01abb00deb974d58d656713f7f527
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2025-01-15 16:15:20 -0700

    added front end structure

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    eslint({
+      lintOnStart: true,
+      failOnError: mode === "production"
+    })
+  ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    },
+  }
+}));
